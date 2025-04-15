833dcb20ae05ac13ec47a86c4e2a4f1d9ba4df2e
trongtinIUH <trantin1973@gmail.com>
2025-04-15 17:06:40 +0700

    áp dụng Fault Tolerance (Circuit Breaker, Retry, Rate Limiter)

--- PaymentService/src/main/java/fit/iuh/paymentservice/controllers/PaymentController.java
+++ PaymentService/src/main/java/fit/iuh/paymentservice/controllers/PaymentController.java
@@ -1,6 +1,8 @@
 package fit.iuh.paymentservice.controllers;
 
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,11 +11,18 @@ public class PaymentController {
 
 
     @PostMapping("/process")
+    @CircuitBreaker(name = "paymentService", fallbackMethod = "fallbackPayment")
+    @Retry(name = "paymentService")
     public String processPayment() {
         // Giả lập xử lý thanh toán
+        if (Math.random() > 0.5) {
+            throw new RuntimeException("Payment failed!");
+        }
         return "Payment processed successfully.";
     }
-
+    public String fallbackPayment(Throwable t) {
+        return "Payment service is currently unavailable. Please try again later.";
+    }
     @PostMapping("/refund")
     public String processRefund() {
         // Giả lập hoàn tiền
